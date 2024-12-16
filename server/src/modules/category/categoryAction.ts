1fc4401e55d6a969824d92a5f68b6521a82a6e5e
REMI DUBUS <remdubus@gmail.com>
2024-12-16 15:10:55 +0100

    feat: add categories

---
+++ server/src/modules/category/categoryAction.ts
@@ -0,0 +1,33 @@
+const categories = [
+  {
+    id: 1,
+    name: "Comédie",
+  },
+  {
+    id: 2,
+    name: "Science-Fiction",
+  },
+];
+
+// Declare the actions
+import type { RequestHandler } from "express";
+
+const browse: RequestHandler = (req, res) => {
+  res.json(categories);
+};
+
+const read: RequestHandler = (req, res) => {
+  const parsedId = Number.parseInt(req.params.id);
+
+  const category = categories.find((p) => p.id === parsedId);
+
+  if (category != null) {
+    res.json(category);
+  } else {
+    res.sendStatus(404);
+  }
+};
+
+// Export them to import them somewhere else
+
+export default { read, browse };
--- server/src/router.ts
+++ server/src/router.ts
@@ -12,15 +12,22 @@ import itemActions from "./modules/item/itemActions";
 router.get("/api/items", itemActions.browse);
 router.get("/api/items/:id", itemActions.read);
 router.post("/api/items", itemActions.add);
+
 /* ************************************************************************* */
+
+import categoryAction from "./modules/category/categoryAction";
+import programActions from "./modules/program/programActions";
+
+router.get("/api/programs", programActions.browse);
+router.get("/api/programs/:id", programActions.read);
+router.get("/api/categories", categoryAction.browse);
+router.get("/api/categories/:id", categoryAction.read);
 
 // Declaration of a "Welcome" route
 
-import programActions from "./modules/program/programActions";
 import sayActions from "./modules/say/sayActions";
 
 router.get("/", sayActions.sayWelcome);
-router.get("/api/programs", programActions.browse);
 
 /* ************************************************************************* */
 
